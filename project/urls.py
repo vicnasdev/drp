60c940406acbf420f469df91c348b48111d0293c
Vic-Nas <nasci.victorio@gmail.com>
2026-02-28 03:17:43 -0500

    Update urlpatterns

--- project/urls.py
+++ project/urls.py
@@ -9,7 +9,7 @@ handler500 = 'core.views.error_handler.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('api/v1/', include('core.api.urls')),
     path('billing/', include('billing.urls')),
     path('help/', include('help.urls')),
     path('', include('core.urls')),
