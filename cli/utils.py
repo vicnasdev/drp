67859c868e55f519cc270d4ec0513689fcf7fd2c
Vic-Nas <nasci.victorio@gmail.com>
2026-03-05 12:39:07 -0500

    add missing console init

--- cli/utils.py
+++ cli/utils.py
@@ -1,7 +1,9 @@
 from rich.console import Console
 
+console = Console()
+
 def spin(func: object, text: str):
     def res(*args):
-        with Console.status(text):
+        with console.status(text):
             func(*args)
     return res
